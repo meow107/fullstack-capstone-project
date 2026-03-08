ce7ccc83773e8788a8615f96b270a97393feaad6
meow <46396000+meow107@users.noreply.github.com>
2026-03-09 05:03:08 +0700

    - Implement the Search Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage  from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -16,7 +21,8 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/detail" element={<DetailsPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path='/app/search' element={SearchPage}/>
         </Routes>
         </>
   );
